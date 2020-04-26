9d81d782d5098dd0c4d2296fc8edbfc97b1c2315
minlaxz <mgmglatt.6991@gmail.com>
2020-04-26 21:09:23 +0630

    encryption and decryption added.

--- laxz.sh
+++ laxz.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-
-help_func(){
+version='1.0.0'
+developer='minlaxz'
+usage(){
     echo "-h , --help , print this help message and exit."
     echo "-b, --brightness, change screen brightness (0.5 to 1.4)."
     echo "-i , --internet , check internet."
     echo "-u, --update , pkg update and upgrade."
+    echo "-z, --zip, zip any given {file(s)} {folder(s)}"
+    echo "-e, --encrypt, encrypt given {file} with aes256"
+    echo "-d, --decrypt, decrypt laxz encrpyted {enc_file} aes256"
 }
 
 internet(){
-    echo "Checking Internet ..."
+    echo -e "Checking internet connection ..."
+    wget -q --spider http://example.com
+    if [ $? -eq 0 ] ; then
+            echo "Internet connection is Noice."
+    else
+            echo -e "[INFO]: Cannot make online stuff!\nExiting ..."
+            exit 0
+    fi
 }
+
 
 brightness(){
     if [[ $1 == "" || $1 < 0.5 || $1 > 1.4 ]]; then
@@ -23,7 +35,7 @@ update(){
     sudo apt update
     read -p 'do you want to upgrade: y/n ' uservar
     if [ $uservar == 'y' ]; then
-        echo "upgrade" #TODO
+        sudo apt upgrade
     else
         echo "user aborded."
     fi
@@ -31,6 +43,11 @@ update(){
 
 encryptor(){
     echo "---enCRYption---"
+    echo "$1"
+}
+
+decryptor(){
+    echo "---deCRYption---"
     echo "$1"
 }
 
@@ -44,17 +61,28 @@ zipper(){
     fi
 }
 
+version(){
+    echo "Version : $version"
+}
+
+developer(){
+    echo "Developed by : $developer"
+}
+
 if [ $# == 0 ]; then
-    echo "Option(s) needed."
-    help_func
+    echo "[-h, --help] for usage."
+    echo "[-v, --version] check version."
 else
     case "$1" in
 
-	-h) help_func ;; 
-	-b) brightness $2 ;; 
-    -z) zipper $* ;;
-	-i) internet ;; 
-    -u) update ;;
+	-h | --help) usage ;; 
+    -v | --version) version ;;
+    -e | --encrypt) encryptor $2 ;;
+    -d | --decrypt) decryptor $s ;;
+	-b | --brightness) brightness $2 ;; 
+	-i | --internet) internet ;; 
+    -u | --update) update ;;
+    -z | --zip) zipper $* ;;
 	*) echo "Option $1 not recognized" ;;
 
 	esac
