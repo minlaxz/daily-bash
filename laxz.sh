d20137833519eebeda9ed0d752d27d3132f9a1b5
minlaxz <mgmglatt.6991@gmail.com>
2020-04-26 21:51:48 +0630

    Added mount and safe remove.

--- laxz.sh
+++ laxz.sh
@@ -1,77 +1,100 @@
 #!/bin/bash
-version='1.0.0'
-developer='minlaxz'
+version='1.0.0' ;
+developer='minlaxz' ;
 usage(){
-    echo "-h , --help , print this help message and exit."
-    echo "-b, --brightness, change screen brightness (0.5 to 1.4)."
-    echo "-i , --internet , check internet."
-    echo "-u, --update , pkg update and upgrade."
-    echo "-z, --zip, zip any given {file(s)} {folder(s)}"
-    echo "-e, --encrypt, encrypt given {file} with aes256"
-    echo "-d, --decrypt, decrypt laxz encrpyted {enc_file} aes256"
+    echo "------------NonRoot------------";
+    echo "-h , --help , print this help message and exit." ;
+    echo "-b, --brightness, change screen brightness (0.5 to 1.3)." ;
+    echo "-i , --internet , check internet connection." ;
+    echo "-z, --zip, zip any given {file(s)} {folder(s)}." ;
+    echo "-e, --encrypt, encrypt given {file} with aes256." ;
+    echo "-d, --decrypt, decrypt laxz encrpyted {enc_file} aes256." ;
+    echo "-rm, --remove, safe remove to .trash unlike built-in `rm`." ;
+    echo "-----------Root-----------------";
+    echo "-u, --update , pkg update and upgrade." ;
+    echo "-m, --mount, mount a network's samba drive."
 }
 
 internet(){
-    echo -e "Checking internet connection ..."
-    wget -q --spider http://example.com
-    if [ $? -eq 0 ] ; then
-            echo "Internet connection is Noice."
-    else
-            echo -e "[INFO]: Cannot make online stuff!\nExiting ..."
-            exit 0
-    fi
+    echo -e "Checking internet connection ..." ;
+    wget -q --spider http://example.com ;
+    if [ $? -eq 0 ] ; 
+    then echo "Internet connection is -Noice-." ;
+    else echo -e "No Internet Connection!" ;
+    exit 0 ; fi
 }
 
 
 brightness(){
-    if [[ $1 == "" || $1 < 0.5 || $1 > 1.4 ]]; then
-        echo "brightness $1 {value error}."
+    if [[ $1 == "" || $1 < 0.5 || $1 > 1.3 ]]; 
+    then echo "brightness $1 {value error}."; 
     else
-        echo "brightness set"
-    fi
+    port=$(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1);
+    echo "Port is detected : $port";
+    xrandr --output $port --brightness $1;
+    echo "set.";
+    fi;
 }
 
 update(){
-    sudo apt update
-    read -p 'do you want to upgrade: y/n ' uservar
-    if [ $uservar == 'y' ]; then
-        sudo apt upgrade
-    else
-        echo "user aborded."
-    fi
+    if [ $(id -u) != "0" ] ;
+    then echo "Please run 'update option' as root" ;
+    else sudo apt update ;
+    read -p 'do you want to upgrade: y/n ' uservar ;
+        if [ $uservar -eq 'y' ];
+        then sudo apt upgrade ;
+        else echo "user aborded." ; fi;
+    fi;
 }
 
 encryptor(){
-    echo "---enCRYption---"
-    echo "$1"
+    echo "---enCRYption---" ;
+    echo "$1" ;
 }
 
 decryptor(){
-    echo "---deCRYption---"
-    echo "$1"
+    echo "---deCRYption---" ;
+    echo "$1" ;
 }
 
 zipper(){
-    timenow=$(date +"%Y-%m-%d-%T")
-    echo "---zip---"
-    zip -r /home/$USER/laxz-$timenow.zip $*
-    read -p 'do you want to encrypt: y/n ' uservar
-    if [ $uservar == 'y' ]; then
-        encryptor /home/$USER/laxz-$timenow.zip
-    fi
+    timenow=$(date +"%Y-%m-%d-%T") ;
+    echo "---zip---" ;
+    zip -r /home/$USER/laxz-$timenow.zip "$@" ;
+    read -p 'do you want to encrypt: y/n ' uservar ;
+    if [ $uservar == 'y' ];
+    then encryptor /home/$USER/laxz-$timenow.zip ; fi
+}
+
+remover(){
+    mv "$@" /home/$USER/.trash ;
+    echo "removed to Trash." ;
+}
+
+mounter(){
+    if [ $(id -u) != "0" ] ;
+    then echo "Please run 'update option' as root" ;
+    else
+        if [ $1 != "" ] ; then
+        mountpoint=$1
+        sudo mount.cifs //192.168.0.10/samba_share $mountpoint -o user=laxzs,rw,pass=laxzsmb
+        else
+        echo "Specify the mount point."
+        fi;
+    fi;
 }
 
 version(){
-    echo "Version : $version"
+    echo "Version : $version" ;
 }
 
 developer(){
-    echo "Developed by : $developer"
+    echo "Developed by : $developer" ;
 }
 
-if [ $# == 0 ]; then
-    echo "[-h, --help] for usage."
-    echo "[-v, --version] check version."
+if [ $# -eq 0 ]; then
+    echo "[-h, --help] for usage." ;
+    echo "[-v, --version] check version." ;
 else
     case "$1" in
 
@@ -83,6 +106,8 @@ else
 	-i | --internet) internet ;; 
     -u | --update) update ;;
     -z | --zip) zipper $* ;;
+    -m | --mount) mounter $2 $3;;
+    -rm | --remove) remover $* ;;
 	*) echo "Option $1 not recognized" ;;
 
 	esac
