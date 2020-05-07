27406b9b8caf72c0b0d1357f30be9b76e051fc7c
minlaxz <mgmglatt.6991@gmail.com>
2020-05-07 21:59:21 +0630

    port and ip-address scan featur added.

--- network-handling.sh
+++ network-handling.sh
@@ -8,17 +8,50 @@ internet(){
     fi
 }
 
+port_ip(){
+    read -p "SCAN (p)ort or (i)paddress ? : " uservar
+    case $uservar in 
+        [pP]*)
+        #port
+        read -p "which port ? > default all LISTEN : " uservar
+        if [ $uservar != "" ] ; then
+        sudo lsof -i:$uservar
+    #sudo ss -tulpn
+    #sudo netstat -tupln | grep LISTEN
+        else
+        sudo lsof -i -P -n | grep LISTEN
+        fi
+        ;;
+
+    [iI]*)
+    #ip-address
+    read -p "IPADDRESS : " uservar
+    sudo nmap -sTU -O $uservar
+    ;;
+    
+    *)
+    echo "Aborded."
+    ;;
+
+esac
+
+
+}
+
 init(){
     PS3="Options for network : "
-    options=("internet" "port" "Adios")
+    options=("internet" "port-ipAddress-scan" "Adios")
     
     select opt in "${options[@]}"
     do
         case $opt in
             "internet")
-            internet ; break;;
-            "port")
-            echo "#TODO" ;;
+            internet ; 
+            break
+            ;;
+            "port-ipAddress-scan")
+            port_ip ;
+            ;;
             "Adios" | "q")
                 clear
                 echo "you chose choice $REPLY which is $opt !" '("Exit")'
