79ea441e93f1ef57fc3bbd37039b3de1f22932f8
minlaxz <mgmglatt.6991@gmail.com>
2020-05-09 22:12:07 +0630

    optimized

--- hardware-handling.sh
+++ hardware-handling.sh
@@ -1,11 +1,14 @@
+user_files=/home/$USER/git-in-sync/workspace-bash/daily-bash
+config_file=$user_files/laxz_config
+source $config_file
+
 monitor_brightness(){
-    if [[ $1 == "" || $1 < 0.5 || $1 > 1.3 ]];
-    then echo "brightness $1 {value error}.";
+    if [[ $2 == "" || $2 < 0.5 || $2 > 1.3 ]];
+    then echo "brightness $2 {value error}.";
     else
-        port=$(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1);
-        echo "Port is detected : $port";
-        xrandr --output $port --brightness $1;
-        echo "set.";
+        xrandr --output $1 --brightness $2;
+        sed -i "s/brightness=[^ ]*/brightness=$2/" $config_file
+        echo "all set.";
     fi;
 }
 init(){
@@ -16,16 +19,22 @@ init(){
     do
         case $opt in
             "monitor")
-                read -p 'set brightness 0.5 ~ 1.3 : ' brightness ;
-                monitor_brightness $brightness; break;;
+                #echo "current brightness is " `cut -f 2 -d ' ' laxz_config`
+                port=$(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1);
+                echo "port detected : $port";
+                echo "current brightness : $brightness"
+                read -p 'set brightness 0.5 ~ 1.3 : ' br ;
+                monitor_brightness $port $br;
+            break;;
             "resource")
-                echo "#TODO" ;;
+                echo "#TODO" 
+            break;;
             "Adios" | "q")
                 clear
-                echo "you chose choice $REPLY which is $opt !" '("Exit")'
-                break
-            ;;
-            *) echo "invalid option $REPLY"; break;;
+                echo "you chose choice $REPLY which is $opt !" '("Exit")'    
+            break;;
+            *) echo "invalid option $REPLY";
+            break;;
         esac
     done
 }
