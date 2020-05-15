af1acc8e27d165dd43dca901dd6639f381e1f5cf
minlaxz <mgmglatt.6991@gmail.com>
2020-05-15 20:54:10 +0630

    encrypted file checker optimized.

--- laxz.sh
+++ laxz.sh
@@ -7,14 +7,14 @@ user_files=/home/$USER/git-in-sync/workspace-bash/daily-bash
 config_file=$user_files/laxz_config
 source $config_file
 
-now=`date "+%Y-%m-%d-%T"`
+now=$(date "+%Y-%m-%d-%T")
 echo "last used $last_used"
 sed -i "s/last_used=[^ ]*/last_used=$now/" $config_file
 
 AESKEY=$user_files/symme.key
 
-usage(){
-    echo "      --help      print laxz help message and exit." 
+usage() {
+    echo "      --help      print laxz help message and exit."
     echo "      --version   print laxz version."
     echo "      --reset     no laxz hagas esto."
     echo "      --status    laxz status."
@@ -25,128 +25,132 @@ usage(){
     echo "-rm   --remove    safe remove files and directories."
     echo ""
     echo "-h    --hardware  handle the hardware parts."
-    echo "-n    --network   handle the networks." 
+    echo "-n    --network   handle the networks."
     echo "-v    --virtual   virtual machine options."
 
-    echo "-z    --zip       zip any given file(s) or folder(s)." 
-    echo "-e    --encrypt   encrypt given {file} with aes256." 
-    echo "-d    --decrypt   decrypt laxz encrpyted {enc_file} aes256." 
+    echo "-z    --zip       zip any given file(s) or folder(s)."
+    echo "-e    --encrypt   encrypt given {file} with aes256."
+    echo "-d    --decrypt   decrypt laxz encrpyted {enc_file} aes256."
 
-    echo "-p    --pkg       package update and upgrade." 
+    echo "-p    --pkg       package update and upgrade."
     echo "-m    --mount     mount a network's samba drive."
 }
-integrity(){
+integrity() {
     echo "Original : $2 , CHECKSUM : $1 "
     echo "Modified : $4 , CHECKSUM : $3 "
 }
-encryptor(){
-    echo "---enCRYption---" ;
+encryptor() {
+    echo "---enCRYption---"
     if [ $# -eq 0 ]; then
         echo "laxz -e {file} or {file_path}"
     else
         org_file=$1
         if [ ! -f $org_file ]; then
-        echo "404."
-        exit
+            echo "404."
+            exit
         fi
-        echo "encrypting file : '$org_file' with aes-256." ;
-        read -p "encrypt with (p)assword or (k)eyfile ? : " uservar;
+        echo "encrypting file : '$org_file' with aes-256."
+        read -p "encrypt with (p)assword or (k)eyfile ? : " uservar
         case $uservar in
-            [Pp]*) 
+        [Pp]*)
             #Password
-            openssl aes-256-cbc -salt -pbkdf2 -in $org_file -out $org_file.loc;
-            integrity `sha256sum $org_file` $(sha256sum $org_file.loc);
-            echo "enCRYption Done." ;;
-            
-            [Kk]*)
+            openssl aes-256-cbc -salt -pbkdf2 -in $org_file -out $org_file.loc
+            integrity $(sha256sum $org_file) $(sha256sum $org_file.loc)
+            echo "enCRYption Done."
+            ;;
+
+        [Kk]*)
             #keyfile
-            if [ ! -f  $AESKEY ] ; then
-            echo "AESKey not found!\nGenerating new one.\n";
-            openssl rand 256 > symme.key ;
-            openssl enc -aes-256-cbc -salt -pbkdf2 -in $org_file -out $org_file.locs -k $AESKEY ;
-            else openssl enc -aes-256-cbc -salt -pbkdf2 -in $org_file -out $org_file.locs -k $AESKEY ; 
+            if [ ! -f $AESKEY ]; then
+                echo "AESKey not found!\nGenerating new one.\n"
+                openssl rand 256 >symme.key
+                openssl enc -aes-256-cbc -salt -pbkdf2 -in $org_file -out $org_file.los -k $AESKEY
+            else
+                openssl enc -aes-256-cbc -salt -pbkdf2 -in $org_file -out $org_file.los -k $AESKEY
             fi
-            integrity `sha256sum $org_file` $(sha256sum $org_file.locs);
-            echo "enCRYption Done.";;
-            
-            *) echo "Aborded." ;;
+            integrity $(sha256sum $org_file) $(sha256sum $org_file.locs)
+            echo "enCRYption Done."
+            ;;
+
+        *) echo "Aborded." ;;
         esac
     fi
 }
 
-decryptor(){
-    echo "---deCRYption---" ;
+decryptor() {
+    echo "---deCRYption---"
     if [ $# -eq 0 ]; then
         echo "laxz -d {file} or {file_path}"
     else
         locked_file=$1
         if [ ! -f $locked_file ]; then
-        echo "404. file input error"
-        exit
-        elif [[ $locked_file == *.loc ]]; then
+            echo "404. file input error"
+            exit
+        elif [[ ${locked_file: -4} == ".loc" ]]; then
             echo "encrypted with PASSWORD"
-            openssl aes-256-cbc -d -salt -pbkdf2 -in $locked_file -out ${locked_file/.loc}
-            integrity `sha256sum $locked_file` `sha256sum ${locked_file/.loc}`
-        elif [[ $locked_file==*.locs ]] ; then
+            openssl aes-256-cbc -d -salt -pbkdf2 -in $locked_file -out ${locked_file/.loc/}
+            integrity $(sha256sum $locked_file) $(sha256sum ${locked_file/.loc/})
+        elif [[ ${locked_file: -4} == ".los" ]]; then
             echo "encrypted with KEY_FILE"
-            if [ ! -f $AESKEY ] ; then
-            echo "THIS IS FETAL, NO AES_KEY FOUND TO UNLOCK!"
-            echo "make sure 'symme.key' file is accessible."
+            if [ ! -f $AESKEY ]; then
+                echo "THIS IS FETAL, NO AES_KEY FOUND TO UNLOCK!"
+                echo "make sure 'symme.key' file is accessible."
             else
-            openssl enc -aes-256-cbc -d -salt -pbkdf2 -in $locked_file -out ${locked_file/.locs} -k $AESKEY
-            integrity `sha256sum $locked_file` `sha256sum ${locked_file/.locs}`
+                openssl enc -aes-256-cbc -d -salt -pbkdf2 -in $locked_file -out ${locked_file/.locs/} -k $AESKEY
+                integrity $(sha256sum $locked_file) $(sha256sum ${locked_file/.locs/})
             fi
         else
-            echo "not a locked file."
+            echo "[fetal] : this is NOT locked by laxz."
             exit
         fi
     fi
 }
 
-zipper(){
-    timenow=$(date +"%Y-%m-%d-%T") ;
-    echo "---zip---" ;
-    zip -r /home/$USER/laxz-$timenow.zip "$@" ;
-    read -p "do you want to encrypt: y/n " uservar ;
-    if [ $uservar == 'y' ];
-    then encryptor /home/$USER/laxz-$timenow.zip ; 
-    else printf "Cancel.";
+zipper() {
+    timenow=$(date +"%Y-%m-%d-%T")
+    echo "---zip---"
+    zip -r /home/$USER/laxz-$timenow.zip "$@"
+    read -p "do you want to encrypt: y/n " uservar
+    if [ $uservar == 'y' ]; then
+        encryptor /home/$USER/laxz-$timenow.zip
+    else
+        printf "Cancel."
     fi
 }
 
-mounter(){
-    if [ $(id -u) != "0" ] ;
-    then echo "Please run 'update option' as root" ;
+mounter() {
+    if [ $(id -u) != "0" ]; then
+        echo "Please run 'update option' as root"
     else
-        if [ $1 != "" ] ; then
+        if [ $1 != "" ]; then
             mountpoint=$1
             sudo mount.cifs //192.168.0.10/samba_share $mountpoint -o user=laxzs,rw,pass=laxzsmb
         else
             echo "Specify the mount point."
-        fi;
-    fi;
+        fi
+    fi
 }
 
-iFunc(){
-flag=$1
-value=$2
+iFunc() {
+    flag=$1
+    value=$2
     case "$flag" in
-    "--monitor" | "-m" )
-    #error -1, >= 10
-        if [[ $value == "" || $value < 0.5 || $value > 1.3 ]] ; then #|| $value =~ ^[-+]?[0-9]+$
-        echo "brightness $value {value error}."
+    -m | --monitor)
+        #error -1, >= 10
+        if [[ $value == "" || $value < 0.5 || $value > 1.3 ]]; then #|| $value =~ ^[-+]?[0-9]+$
+            echo "brightness $value {value error}."
         else
             if [[ $brightness == $value ]]; then
-            echo "Brightness is already set to <$value>."
+                echo "Brightness is already set to <$value>."
             else
-            echo "previous brightness : $brightness"
-            echo "current brightess : $value"
-            port=$(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1);
-            echo "port detected : $port";
-            xrandr --output $port --brightness $value;
-            sed -i "s/brightness=[^ ]*/brightness=$value/" $config_file
-            echo "all set."
-            fi;
+                echo "previous brightness : $brightness"
+                echo "current brightess : $value"
+                port=$(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1)
+                echo "port detected : $port"
+                xrandr --output $port --brightness $value
+                sed -i "s/brightness=[^ ]*/brightness=$value/" $config_file
+                echo "all set."
+            fi
         fi
         ;;
     *)
@@ -155,87 +159,107 @@ value=$2
     esac
 }
 
-sync(){
-    clear
-    xrandr --output VGA-1 --brightness $brightness;
-    echo "all settings in sync."
+sync() {
+    port=$(xrandr -q | grep ' connected' | head -n 1 | cut -d ' ' -f1)
+    xrandr --output $port --brightness $brightness
+    echo "all set."
 }
 
 if [ $# -eq 0 ]; then
-    echo "[--help] for usage." ;
-    echo "[--version] check version." ;
+    echo "[--help] for usage."
+    echo "[--version] check version."
 else
     case "$1" in
-        
-        --help) usage ;;
-        --version) 
+
+    --help) usage ;;
+    --version)
         echo "$version"
         ;;
-        --developer)
+    --developer)
         echo -e "\a"
         clear
         notify-send 'hidden option' 'Developed by minlaxz'
         ;;
-        --reset) 
+    --reset)
         echo "Reset laxz."
         ;;
-        --status) 
+    --status)
         echo "Brightess is $brightness"
         ;;
-        --sync) 
+    --sync)
         clear
-        xrandr --output VGA-1 --brightness $brightness;
+        xrandr --output VGA-1 --brightness $brightness
         echo "all settings in sync."
         ;;
+    --test)
+        echo "third arg is : ${!3}"
+        #echo "third arg is : ${@:3}"
+        ;;
+    -e | --encrypt) encryptor $2 ;;
+    -d | --decrypt) decryptor $2 ;;
+    -z | --zip) zipper $* ;;
 
-        -e | --encrypt) encryptor $2 ;;
-        -d | --decrypt) decryptor $2 ;;
-        -z | --zip) zipper $* ;;
-
-        -h | --hardware)
+    -h | --hardware)
         if [[ $2 == "" ]]; then
-        bash $user_files/hardware-handling.sh
+            bash $user_files/hardware-handling.sh
         else
-        case "$2" in
-            -m | --monitor ) 
-            if [[ $3 == "" ]]; then
-            read -p "set brightness 0.5 ~ 1.3 : " br ;
-            iFunc $2 $br
-            else
-            iFunc $2 $3
-            fi
-            ;; ##EXTENSIBLE
+            case "$2" in
+            -m | --monitor)
+                if [[ $3 == "" ]]; then
+                    read -p "set brightness 0.5 ~ 1.3 : " br
+                    iFunc $2 $br
+                else
+                    iFunc $2 $3
+                fi
+                ;; ##EXTENSIBLE
             *)
-            echo "$2 is invalid for $1."
-            ;;
-        esac
+                echo "$2 is invalid for $1."
+                ;;
+            esac
         fi
         ;;
-        -n | --network) bash $user_files/network-handling.sh ;;
-        -p | --pkg) bash $user_files/package-handling.sh ;;
-        -v | --virtual) bash $user_files/vm-handling.sh ;;
-
-        
-        -m | --mount) mounter $2 ;;
-        -rm | --remove) 
-        echo "remove function with pain in ass safe." 
-        if [[ `apt list --installed trash-cli 2>/dev/null | wc -l` == 1 ]] ; then
-        sudo apt install trash-cli #Thanks https://github.com/andreafrancia/trash-cli
+    -n | --network)
+        if [[ $2 == "" ]]; then
+            bash $user_files/network-handling.sh
         else
-        #echo $2
-        #trash-put "@$"
-        trash-put "$2"
+            case "$2" in
+            -if)
+                case "$3" in
+                en) ifconfig eno1 | grep inet ;;
+                do) ifconfig docker0 | grep inet ;;
+                lo) ifconfig lo | grep inet ;;
+                esac
+                ;;
+            *)
+                echo "invalid"
+                ;;
+            esac
+        fi
+        ;;
+    -p | --pkg) bash $user_files/package-handling.sh ;;
+    -v | --virtual) bash $user_files/vm-handling.sh ;;
+
+    -m | --mount) mounter $2 ;;
+    -rm | --remove)
+        echo "remove function with pain in ass safe."
+        if [[ $(apt list --installed trash-cli 2>/dev/null | wc -l) == 1 ]]; then
+            sudo apt install trash-cli #Thanks https://github.com/andreafrancia/trash-cli
+        else
+            #echo $2
+            #trash-put "@$"
+            trash-put "$2"
         fi
         echo "removed to Trash."
         ;;
-        -cp | --copy)
+    -cp | --copy)
         echo "copy function with progress verbose."
         if [[ $3 == "" ]]; then
-        echo "insuffcient argument."
+            echo "insuffcient argument."
         else
-        rsync -ah --progress $2 $3
-        fi;;
-        *) echo "$1 isn't laxz's command." ;;
-        
+            rsync -ah --progress $2 $3
+        fi
+        ;;
+    *) echo "$1 isn't laxz's command." ;;
+
     esac
 fi
