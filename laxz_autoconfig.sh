db61b76fb8f67d673e46c2a394ab9a0f576eec46
minlaxz <mgmglatt.6991@gmail.com>
2020-06-08 01:27:23 +0630

    testing script.

--- laxz_autoconfig.sh
+++ laxz_autoconfig.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-general() {
-    echo "installing general libraries."
+# this is coded by minlaxz `github.com/minlaxz`
+# you may do with this what the fuck you want to.
+# nth to say.
+set -e
+
+general_install() {
+    echo "installing general dependencies."
     apt install build-essentials net-tools cmake gcc g++ python3-dev git vim curl
 }
-shell() {
+shell_install() {
     sudo apt install zsh curl
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
-miniconda() {
+miniconda_install() {
     echo "Miniconda Installation."
 
 }
-opencv() {
-    echo "Required build dependencies."
+opencv_install() {
+    echo "installing required build dependencies."
     sudo apt install cmake gcc g++
     echo "to support python3."
     sudo python3-dev python3-numpy
@@ -41,14 +46,34 @@ opencv() {
     #import cv2 as cv
     #print(cv.__version__)
 }
-
+installer(){
+    echo "flag: --install"
+    option=$1
+    case "$option" in
+    general)
+    echo "installing general dependencies..."
+    ;;
+    opencv)
+    echo "installing opencv..."
+    ;;
+    *)
+    echo "[handler]: abort."
+    echo "[handler]: /long_command {install} {option}"
+    echo "[handler]: visit https://minlaxz.web.app/init.html for detail."
+    ;;
+}
 main(){
     flag=$1
     option=$2
     echo "--Welcome to laxz--"
     echo -e "\a"
-    echo "flag: " $flag
-    echo "option: " $option
+    if [[ $flag=='install' ]]; then
+    installer $option
+    else
+    echo "[main]: this do not support anything other than 'install' yet."
+    fi
 }
 
 main "$@"
+
+#https://raw.githubusercontent.com/minlaxz/daily-bash/master/laxz_autoconfig.sh
