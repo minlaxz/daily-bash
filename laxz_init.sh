96e01480bb7f4a2bfdaffcf3b47b9697c60d3ec0
minlaxz <mgmglatt.6991@gmail.com>
2020-08-20 16:42:14 +0630

    1.2.1

--- laxz_init.sh
+++ laxz_init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-lxz_prefix=$HOME/.local/share/lxz.gosh/tmp && \
-mkdir -p $lxz_prefix && \
-curl https://raw.githubusercontent.com/minlaxz/daily-bash/master/laxz_autoconfig.sh --output $lxz_prefix/lxz_init.sh && \
-chmod 744 $lxz_prefix/lxz_init.sh && \
-bash $lxz_prefix/lxz_init.sh
+tmp_prefix=$HOME/.local/share/lxz.tmp && \
+mkdir -p $tmp_prefix && \
+curl https://raw.githubusercontent.com/minlaxz/daily-bash/master/laxz_autoconfig.sh --output $tmp_prefix/lxz_init.sh && \
+chmod 744 $tmp_prefix/lxz_init.sh && \
+bash $tmp_prefix/lxz_init.sh
